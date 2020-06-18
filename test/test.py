26d1675edb85471d01a54c1a13dc87db5efabdc8
Martin Beneš <martinbenes1996@gmail.com>
2020-06-18 12:36:09 +0200

    automated unittests

---
+++ test/test.py
@@ -0,0 +1,17 @@
+import sys
+import unittest
+
+sys.path.append(".")
+
+# === unit tests ===
+# ==================
+
+
+# logging
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level = logging.WARNING)
+
+# run unittests
+if __name__ == "__main__":
+    unittest.main()
