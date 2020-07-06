429badc527fdf8686751a8e9856f7a4c8664e46f
Martin Beneš <martinbenes1996@gmail.com>
2020-07-06 19:08:22 +0200

    manifest removed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
   name = 'covid19poland',
-  version = '0.1.4',
+  version = '0.1.5',
   author = 'Martin Beneš',
   author_email = 'martinbenes1996@gmail.com',
   description = 'Web Scraper for Poland COVID19 data.',
@@ -21,11 +21,11 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   license='MIT',
   url = 'https://github.com/martinbenes1996/covid19poland',
-  download_url = 'https://github.com/martinbenes1996/covid19poland/archive/0.1.4.tar.gz',
+  download_url = 'https://github.com/martinbenes1996/covid19poland/archive/0.1.5.tar.gz',
   keywords = ['2019-nCov', 'poland', 'coronavirus', 'covid-19', 'covid-data', 'covid19-data'],
   install_requires=reqs,
   package_dir={'': '.'},
-  package_data={'': ['data/*.json']},
+  package_data={'': ['data/*.json','data/*.csv']},
   include_package_data=True,
   classifiers=[
     'Development Status :: 4 - Beta',
