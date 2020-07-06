0546a07ebb80f03ca05b7e3ff1c540479403eb59
Martin Beneš <martinbenes1996@gmail.com>
2020-07-06 10:33:11 +0200

    release 12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
   name = 'covid19poland',
-  version = '0.0.10',
+  version = '0.0.12',
   author = 'Martin Beneš',
   author_email = 'martinbenes1996@gmail.com',
   description = 'Web Scraper for Poland COVID19 data.',
@@ -21,11 +21,11 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   license='MIT',
   url = 'https://github.com/martinbenes1996/covid19poland',
-  download_url = 'https://github.com/martinbenes1996/covid19poland/archive/0.0.10.tar.gz',
+  download_url = 'https://github.com/martinbenes1996/covid19poland/archive/0.0.12.tar.gz',
   keywords = ['2019-nCov', 'poland', 'coronavirus', 'covid-19', 'covid-data', 'covid19-data'],
   install_requires=reqs,
   package_dir={'': '.'},
-  package_data={'': ['data/*.csv']},
+  package_data={'': ['data/*.json']},
   include_package_data=True,
   classifiers=[
     'Development Status :: 3 - Alpha',
