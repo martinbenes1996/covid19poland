531eb6fa0fa99bd902f01bc8eeca959f65b6a0f1
Martin Beneš <martinbenes1996@gmail.com>
2020-09-01 09:48:33 +0200

    publish daily exports references too

--- covid19poland/export.py
+++ covid19poland/export.py
@@ -17,6 +17,8 @@ def export(start, end, fname, keys = ["deaths"]):
     
 def export_csv(start, end, fname, keys = ["deaths"]):
     data,filtered,checklist = PolishTwitter.get(start, end, keys=keys)
+    with open(f"data/{fname}_in.json", "w") as fd:
+        json.dump(data, fd, sort_keys=True, indent = 4, separators = (',',": "))
     x = offline.covid_death_cases(data)
     x.to_csv(f"data/{fname}.csv", index = False)
     if checklist:
